24847bcfc722c0130fb83d0ba0935d1bfa216e58
“katsuokaisao” <isaoda0@icloud.com>
2025-08-23 18:47:12 +0900

    ユーザ編集でニックネーム変更時にパスワードの更新が必要になっていたのを直した

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,23 @@ class User < ApplicationRecord
   NICKNAME_MAX_LENGTH = 50
   PASSWORD_MIN_LENGTH = 1
   PASSWORD_MAX_LENGTH = 50
-
   devise :database_authenticatable, :registerable
 
   validates_presence_of     :nickname
   validates_uniqueness_of   :nickname
   validates_length_of       :nickname, minimum: NICKNAME_MIN_LENGTH, maximum: NICKNAME_MAX_LENGTH, allow_blank: true
-  validates_presence_of     :password
-  validates_confirmation_of :password
-  validates_length_of       :password, minimum: PASSWORD_MIN_LENGTH, maximum: PASSWORD_MAX_LENGTH, allow_blank: true
+
+  # user sign up: password, password_confirmation
+  # user account update: current_password, password, password_confirmation
+  with_options if: -> { new_record? || password_update? } do
+    validates_presence_of     :password
+    validates_confirmation_of :password
+    validates_length_of       :password, minimum: PASSWORD_MIN_LENGTH, maximum: PASSWORD_MAX_LENGTH, allow_blank: true
+  end
+
+  private
+
+  def password_update?
+    !new_record? && (password.present? || password_confirmation.present?)
+  end
 end

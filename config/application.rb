9203f566daf8bf23cd4621312af679655bbb9eb2
“katsuokaisao” <isaoda0@icloud.com>
2025-08-23 15:24:21 +0900

    i18n対応

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,8 @@ module BoardApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = [:ja, :en]
   end
 end

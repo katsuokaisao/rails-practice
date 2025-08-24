934761705e1957383fc03a56c2c3887155628d2a
“katsuokaisao” <isaoda0@icloud.com>
2025-08-24 13:58:37 +0900

    プロフィールの変更でもヘッダーを出す

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  layout 'user'
+  layout 'user', except: [:profile, :password]
+  layout 'application', only: [:profile, :password]
 
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]

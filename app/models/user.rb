15f01237000008202f1234d363ca9bbcacf106d3
“katsuokaisao” <isaoda0@icloud.com>
2025-08-25 10:28:10 +0900

    パスワードのバリデーションに長さ制限と許可する文字列の制限を入れる

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,21 @@
 class User < ApplicationRecord
   NICKNAME_MIN_LENGTH = 1
   NICKNAME_MAX_LENGTH = 50
-  PASSWORD_MIN_LENGTH = 1
+  PASSWORD_MIN_LENGTH = 8
   PASSWORD_MAX_LENGTH = 50
+  PASSWORD_REGEX = /\A[!-~]+\z/ # Asciiの印刷可能文字(スペース含まない)のみ
 
   devise :database_authenticatable, :registerable
 
-  validates :nickname, presence: true, uniqueness: true
-  validates :nickname, length: { minimum: NICKNAME_MIN_LENGTH, maximum: NICKNAME_MAX_LENGTH, allow_blank: true }
+  validates :nickname, presence: true, uniqueness: true,
+                       length: { minimum: NICKNAME_MIN_LENGTH, maximum: NICKNAME_MAX_LENGTH, allow_blank: true }
 
   # user sign up: password, password_confirmation
   # user account update: current_password, password, password_confirmation
   with_options if: -> { new_record? || password_update? } do
     validates :password, presence: true, confirmation: true
-    validates :password, length: { minimum: PASSWORD_MIN_LENGTH, maximum: PASSWORD_MAX_LENGTH, allow_blank: true }
+    validates :password, length: { minimum: PASSWORD_MIN_LENGTH, maximum: PASSWORD_MAX_LENGTH },
+                         format: { with: PASSWORD_REGEX }, allow_blank: true
   end
 
   private

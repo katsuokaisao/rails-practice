8bbf805d8ed0d617e876d51002c1b801ce6ae56e
“katsuokaisao” <isaoda0@icloud.com>
2025-08-25 09:13:04 +0900

    ログイン・ログアウト後のリダイレクトパスを変更した

--- app/controllers/moderators/sessions_controller.rb
+++ app/controllers/moderators/sessions_controller.rb
@@ -24,11 +24,20 @@ module Moderators
     #   super
     # end
 
-    # protected
+    protected
 
     # If you have extra params to permit, append them to the sanitizer.
     # def configure_sign_in_params
     #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
     # end
+
+    # TODO: 審査一覧画面できたらそこに飛ばす
+    def after_sign_in_path_for(resource)
+      root_path
+    end
+
+    def after_sign_out_path_for(resource)
+      new_moderator_session_path
+    end
   end
 end

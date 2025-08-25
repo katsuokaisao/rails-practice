1fedc8b1b82a04d65026fe45e1616820bd4763ff
“katsuokaisao” <isaoda0@icloud.com>
2025-08-25 19:21:23 +0900

    ホーム関連のコントローラー、ヘルパー、およびビューを削除

--- app/controllers/home_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# ホームコントローラー
-#
-# ログインの動作確認用のページのために一時的に設置。
-class HomeController < ApplicationController
-  def index; end
-end
--- app/helpers/home_helper.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# ホームヘルパー
-#
-# ホーム画面で使用するビューヘルパーメソッドを提供する。
-# ホーム関連の表示ロジックや共通処理を含む。
-module HomeHelper
-end

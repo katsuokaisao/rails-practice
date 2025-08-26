dac92515b3051b7821d725e259aa910e38020aca
“katsuokaisao” <isaoda0@icloud.com>
2025-08-26 14:55:01 +0900

    hidden_reason_typeとhidden_reason_textのnull制約を変更

--- db/schemas/comments.schema.rb
+++ db/schemas/comments.schema.rb
@@ -6,8 +6,8 @@ create_table 'comments', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4' do |t|
   t.text     'content',            null: false
   t.integer  'current_version_no', null: false
   t.boolean  'hidden',             null: false, default: false
-  t.string   'hidden_reason_type', null: false
-  t.text     'hidden_reason_text', null: false
+  t.string   'hidden_reason_type', null: true
+  t.text     'hidden_reason_text', null: true
   t.datetime 'created_at',         null: false
   t.datetime 'updated_at',         null: false
 

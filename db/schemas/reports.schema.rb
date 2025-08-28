b1b9a43ffe7d5859723dfa25c9a467633ed1afe3
“katsuokaisao” <isaoda0@icloud.com>
2025-08-28 17:01:02 +0900

    reports.schema.rb のカラムの整形を改善

--- db/schemas/reports.schema.rb
+++ db/schemas/reports.schema.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 create_table 'reports', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4' do |t|
-  t.string   'target_type',    null: false, comment: "(enum: 'comment' | 'user')"
-  t.bigint   'target_user_id', null: true
+  t.string   'target_type',       null: false, comment: "(enum: 'comment' | 'user')"
+  t.bigint   'target_user_id',    null: true
   t.bigint   'target_comment_id', null: true
-  t.bigint   'reporter_id',    null: false
-  t.string   'reason_type',    null: false
-  t.text     'reason_text',    null: false
-  t.datetime 'created_at',     null: false
-  t.datetime 'updated_at',     null: false
+  t.bigint   'reporter_id',       null: false
+  t.string   'reason_type',       null: false
+  t.text     'reason_text',       null: false
+  t.datetime 'created_at',        null: false
+  t.datetime 'updated_at',        null: false
 
   t.index ['target_user_id'], name: 'idx_reports_target_user_id'
   t.index ['target_comment_id'], name: 'idx_reports_target_comment_id'

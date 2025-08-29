ba32b510ba290d1661406d42e960269322738673
“katsuokaisao” <isaoda0@icloud.com>
2025-08-29 21:16:31 +0900

    コメント非表示にするためのビュー側の変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,16 @@ module ApplicationHelper
   def data_action_name
     "#{action_name}_action"
   end
+
+  def display_comment_content(comment, current_user)
+    if comment.hidden?
+      if comment.author == current_user
+        content_tag(:p, '規約違反の可能性があるため、あなたのコメントは公開画面から非表示になりました。', class: ['hidden-comment-warning'])
+      else
+        content_tag(:p, 'このコメントは非表示です。', class: ['hidden-comment-info'])
+      end
+    else
+      content_tag(:p, comment.content, class: ['comment-content'])
+    end
+  end
 end

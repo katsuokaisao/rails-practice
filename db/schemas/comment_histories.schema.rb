cca3652db5c014c54d19ed46b1896e99347a0d71
“katsuokaisao” <isaoda0@icloud.com>
2025-08-31 09:53:56 +0900

    コメント履歴テーブルのカラムの整形を修正

--- db/schemas/comment_histories.schema.rb
+++ db/schemas/comment_histories.schema.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 create_table 'comment_histories', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4' do |t|
-  t.bigint   'comment_id', null: false
-  t.bigint   'topic_id', null: false
+  t.bigint   'comment_id',       null: false
+  t.bigint   'topic_id',         null: false
   t.bigint   'author_id',        null: false
-  t.integer  'version_no',       null: false
   t.text     'content',          null: false
+  t.integer  'version_no',       null: false
   t.datetime 'created_at',       null: false
 
   t.index %w[comment_id version_no], name: 'idx_comment_histories_comment_id', unique: true

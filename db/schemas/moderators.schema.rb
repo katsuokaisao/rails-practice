aa4fe8f0036560002feb7271581f0c8d0c5bd91e
“katsuokaisao” <isaoda0@icloud.com>
2025-08-31 15:24:41 +0900

    モデレーターおよびユーザーのスキーマからretired_atカラムを削除

--- db/schemas/moderators.schema.rb
+++ db/schemas/moderators.schema.rb
@@ -4,7 +4,6 @@ create_table 'moderators', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4' do |
   t.string   'nickname',           null: false
   t.string   'encrypted_password', null: false
   t.string   'time_zone',          null: false, default: 'Tokyo'
-  t.datetime 'retired_at',         null: true
   t.datetime 'created_at',         null: false
   t.datetime 'updated_at',         null: false
 

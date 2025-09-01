38f2fafea2ccd459cb57092ad0710c470fc5ad14
“katsuokaisao” <isaoda0@icloud.com>
2025-09-01 09:17:32 +0900

    新規登録後の遷移先をトップ画面にする

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -116,5 +116,9 @@ module Users
       kind = params.dig(:user, :update_kind)
       kind.present? ? kind.to_sym : nil
     end
+
+    def after_sign_up_path_for(_resource)
+      root_path
+    end
   end
 end

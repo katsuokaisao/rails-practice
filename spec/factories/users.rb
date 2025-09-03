e164e70243ea8fa6ddb379201b14f73aa5fee5f6
“katsuokaisao” <isaoda0@icloud.com>
2025-09-03 12:21:40 +0900

    ユーザーファクトリに:suspendedトレイトを追加: ユーザー作成時に1ヶ月後にアカウントを停止する処理を追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,11 @@ FactoryBot.define do
     sequence(:nickname) { |n| "test#{n}" }
     password { 'password' }
     password_confirmation { 'password' }
+
+    trait :suspended do
+      after(:create) do |user|
+        user.suspend!(1.month.from_now)
+      end
+    end
   end
 end

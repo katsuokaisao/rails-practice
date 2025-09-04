ca3ba05b2a8418c68035dfd08acf6479ac7ef136
“katsuokaisao” <isaoda0@icloud.com>
2025-09-04 18:11:32 +0900

    不要なエラーハンドリングメソッドを削除し、コードを整理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,21 +10,6 @@ class ApplicationController < ActionController::Base
   # 403
   rescue_from Authorization::NotAuthorizedError, with: :render_forbidden
 
-  # 404
-  rescue_from ActiveRecord::RecordNotFound,               with: :render_not_found
-  rescue_from AbstractController::ActionNotFound,         with: :render_not_found
-
-  # 406
-  rescue_from ActionController::UnknownFormat,            with: :render_not_acceptable
-
-  # 409
-  rescue_from ActiveRecord::RecordNotUnique,              with: :render_conflict
-  rescue_from ActiveRecord::Deadlocked,                   with: :render_conflict
-  rescue_from ActiveRecord::LockWaitTimeout,              with: :render_conflict
-
-  # 422
-  rescue_from ActiveRecord::RecordInvalid,                with: :render_unprocessable_entity
-
   def only_user_signed_in?
     user_signed_in? && !moderator_signed_in?
   end
@@ -41,27 +26,5 @@ class ApplicationController < ActionController::Base
 
   def render_forbidden
     render file: Rails.public_path.join('403.html'), status: :forbidden, layout: false, content_type: 'text/html'
-  end
-
-  def render_not_found
-    render file: Rails.public_path.join('404.html'), status: :not_found, layout: false, content_type: 'text/html'
-  end
-
-  def render_not_acceptable
-    render file: Rails.public_path.join('406.html'), status: :not_acceptable, layout: false, content_type: 'text/html'
-  end
-
-  def render_conflict
-    render file: Rails.public_path.join('409.html'), status: :conflict, layout: false, content_type: 'text/html'
-  end
-
-  def render_unprocessable_entity
-    render file: Rails.public_path.join('422.html'),
-           status: :unprocessable_content, layout: false, content_type: 'text/html'
-  end
-
-  def render_internal_server_error
-    render file: Rails.public_path.join('500.html'),
-           status: :internal_server_error, layout: false, content_type: 'text/html'
   end
 end

8c05dde21e6368f17a2d4ad67c4818507cc929b3
“katsuokaisao” <isaoda0@icloud.com>
2025-09-08 17:43:29 +0900

    Decisionモデルのバリデーションを整理し、propagate_to_similar_reports

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -26,14 +26,16 @@ class Decision < ApplicationRecord
   belongs_to :report
   belongs_to :moderator, class_name: 'Moderator', foreign_key: 'decided_by', inverse_of: :decisions
   enum :decision_type, { reject: 'reject', hide_comment: 'hide_comment', suspend_user: 'suspend_user' },
-       prefix: true, validates: true
+       prefix: true, validate: true
 
   validates :report_id, uniqueness: true
   validates :decision_type, presence: true
-  validate :validate_decision_type_with_report_type
-  validate :validate_suspension_until
   validates :note, length: { maximum: 2000 }, allow_blank: true
-  validate :suspension_until_must_be_in_future
+  validate :target_type_must_be_comment_report, if: :decision_type_hide_comment?
+  validate :target_type_must_be_user_report, if: :decision_type_suspend_user?
+  validate :suspension_until_presence, if: :decision_type_suspend_user?
+  validate :suspension_until_absence, unless: :decision_type_suspend_user?
+  validate :suspension_until_future, if: -> { suspension_until.present? && decision_type_suspend_user? }
 
   def execute!
     ActiveRecord::Base.transaction do
@@ -43,79 +45,79 @@ class Decision < ApplicationRecord
     propagate_to_similar_reports!
   end
 
+  def report_type
+    report.target_type
+  end
+
   private
 
   def apply_effect!
     case decision_type
     when 'hide_comment'
-      report.target_comment.hide_by_decision!(self)
+      hide_comment!
     when 'suspend_user'
-      report.target_user.suspend!(suspension_until)
+      suspend_user!
     end
+  end
+
+  def hide_comment!
+    report.target_comment.hide_by_decision!(self)
+  end
+
+  def suspend_user!
+    report.target_user.suspend!(suspension_until)
   end
 
   def propagate_to_similar_reports!
-    case decision_type
-    when 'hide_comment'
-      propagate_to_similar_content_reports!
-    when 'suspend_user'
-      propagate_to_similar_user_reports!
-    end
-  end
-
-  def propagate_to_similar_content_reports!
-    similar_reports = Report.same_comment_as(report.target_comment_id).without_report(report)
+    similar_reports = case report_type
+                      when 'comment'
+                        similar_content_reports
+                      when 'user'
+                        similar_user_reports
+                      end
 
     similar_reports.each do |similar_report|
       next if similar_report.reviewed?
 
       Decision.create!(
         report: similar_report,
-        decision_type: 'hide_comment',
-        note: "自動作成: 関連する通報 ##{report.id} の審査結果に基づく",
-        moderator: moderator
+        decision_type: decision_type,
+        note: auto_note,
+        moderator: moderator,
+        suspension_until: decision_type_suspend_user? ? suspension_until : nil
       )
     end
   end
 
-  def propagate_to_similar_user_reports!
-    similar_reports = Report.same_user_as(report.target_user_id).without_report(report)
-
-    similar_reports.each do |similar_report|
-      next if similar_report.reviewed?
-
-      Decision.create!(
-        report: similar_report,
-        decision_type: 'suspend_user',
-        note: "自動作成: 関連する通報 ##{report.id} の審査結果に基づく",
-        suspension_until: suspension_until,
-        moderator: moderator
-      )
-    end
+  def similar_content_reports
+    Report.same_comment_as(report.target_comment_id).without_report(report)
   end
 
-  def validate_decision_type_with_report_type
-    return unless report
-
-    case report.target_type
-    when 'comment'
-      errors.add(:decision_type, :invalid_for_comment_report) if decision_type == 'suspend_user'
-    when 'user'
-      errors.add(:decision_type, :invalid_for_user_report) if decision_type == 'hide_comment'
-    end
+  def similar_user_reports
+    Report.same_user_as(report.target_user_id).without_report(report)
   end
 
-  def validate_suspension_until
-    if decision_type == 'suspend_user' && suspension_until.blank?
-      errors.add(:suspension_until, :required_for_suspension)
-    elsif decision_type != 'suspend_user' && suspension_until.present?
-      errors.add(:suspension_until, :not_allowed_for_non_suspension)
-    end
+  def auto_note
+    "自動作成: 関連する通報 ##{report.id} の審査結果に基づく"
   end
 
-  def suspension_until_must_be_in_future
-    return if suspension_until.blank? || suspension_until.future?
+  def target_type_must_be_comment_report
+    errors.add(:report, :invalid_for_comment_report) if report && report.target_type != 'comment'
+  end
 
-    errors.add(:suspension_until, :must_be_in_future)
+  def target_type_must_be_user_report
+    errors.add(:report, :invalid_for_user_report) if report && report.target_type != 'user'
+  end
+
+  def suspension_until_presence
+    errors.add(:suspension_until, :required_for_suspension) if suspension_until.blank?
+  end
+
+  def suspension_until_absence
+    errors.add(:suspension_until, :not_allowed_for_non_suspension) if suspension_until.present?
+  end
+
+  def suspension_until_future
+    errors.add(:suspension_until, :must_be_in_future) unless suspension_until.future?
   end
 end

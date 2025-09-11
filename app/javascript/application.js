7758f35f6656e0b2526c4d63ea0837ce9bdbd17c
“katsuokaisao” <isaoda0@icloud.com>
2025-09-11 18:03:59 +0900

    不要なコメントの削除

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,24 +2,3 @@
 
 import "@hotwired/turbo-rails"
 import "controllers"
-
-// function initDropdown() {
-//   const dropdownTrigger = document.querySelector('.dropdown-trigger');
-//   const dropdownMenu = document.querySelector('.dropdown-menu');
-
-//   if (dropdownTrigger && dropdownMenu) {
-//     dropdownTrigger.addEventListener('click', function(e) {
-//       e.preventDefault();
-//       dropdownMenu.classList.toggle('show');
-//     });
-
-//     document.addEventListener('click', function(e) {
-//       if (!dropdownTrigger.contains(e.target) && !dropdownMenu.contains(e.target)) {
-//         dropdownMenu.classList.remove('show');
-//       }
-//     });
-//   }
-// }
-
-// document.addEventListener('turbo:load', initDropdown);
-// document.addEventListener('DOMContentLoaded', initDropdown);

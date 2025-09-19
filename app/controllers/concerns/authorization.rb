a0eaba30907f0ab385aee1321c1a862f4c4a99df
“katsuokaisao” <isaoda0@icloud.com>
2025-09-19 18:42:22 +0900

    認可失敗時に誰が何をしようとしたかをログに出すようにする

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -9,6 +9,18 @@ module Authorization
     policy = policy(record)
     return true if policy.public_send("#{action_name}?")
 
+    actor_info = if moderator_signed_in?
+                   "Moderator #{current_moderator.id}"
+                 elsif user_signed_in?
+                   "User #{current_user.id}"
+                 else
+                   'Anonymous'
+                 end
+    action_info = "#{controller_name.classify}Controller##{action_name}"
+    msg = "Authorization Failure: #{actor_info} attempted to #{action_info}"
+    msg += " on Record #{record.class.name}##{record.id}" if record.present?
+
+    Rails.logger.warn msg
     raise NotAuthorizedError
   end
 

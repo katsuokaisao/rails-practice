b102cc026d2f314d5220cf71dd88b00e65acc68b
“katsuokaisao” <isaoda0@icloud.com>
2025-09-21 16:51:39 +0900

    decisionsテーブルにcreated_atとreport_idの複合インデックスを追加

--- db/schemas/decisions.schema.rb
+++ db/schemas/decisions.schema.rb
@@ -8,8 +8,9 @@ create_table 'decisions', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4' do |t
   t.datetime 'suspended_until', null: true
   t.datetime 'created_at', null: false
 
-  t.index ['report_id'],  name: 'idx_decisions_report_id', unique: true
-  t.index ['decided_by'], name: 'idx_decisions_decided_by'
+  t.index ['report_id'],                name: 'idx_decisions_report_id', unique: true
+  t.index ['decided_by'],               name: 'idx_decisions_decided_by'
+  t.index %w[created_at report_id], name: 'idx_decisions_created_at_report_id'
 end
 
 add_foreign_key 'decisions', 'reports',    column: 'report_id'

7078130257dc48883b7a8d113132f19e3f81f299
“katsuokaisao” <isaoda0@icloud.com>
2025-10-26 13:49:23 +0900

    SampleCreatorにTenantMembershipの作成を実装

--- lib/sample_creator.rb
+++ lib/sample_creator.rb
@@ -9,6 +9,7 @@ class SampleCreator
     create_tenants
     create_users
     create_suspend_users
+    create_tenant_memberships
     create_moderators
     create_topics
     create_comments
@@ -64,6 +65,68 @@ class SampleCreator
   def create_suspend_users
     5.times do
       FactoryBot.create(:user, :suspended)
+    end
+  end
+
+  def create_tenant_memberships
+    tenants = Tenant.all.to_a
+    users = User.all.to_a
+
+    # 日本語の表示名候補
+    display_names = %w[
+      山田太郎 鈴木花子 田中一郎 佐藤美咲 高橋健太
+      渡辺優子 伊藤大輔 中村舞 小林拓也 加藤愛
+      吉田悠太 山本さくら 佐々木隼人 松本美優 井上翔
+      木村結衣 林雄大 斉藤琴音 清水陽介 森本彩花
+      たろちゃん はなちゃん けんけん まいまい ゆうちゃん
+      taro_y hanako_s ken_t mai_n yuta_y
+    ]
+
+    # 各テナントに10〜15人のメンバーを追加
+    tenants.each do |tenant|
+      member_count = rand(10..15)
+      selected_users = users.sample(member_count)
+      used_names = []
+
+      selected_users.each do |user|
+        # このテナント内で未使用の表示名を選択
+        available_names = display_names - used_names
+        display_name = available_names.sample || "ユーザー#{rand(1000..9999)}"
+
+        TenantMembership.create!(
+          tenant: tenant,
+          user: user,
+          display_name: display_name
+        )
+
+        used_names << display_name
+      end
+    end
+
+    # 一部のユーザーは複数のテナントに所属させる
+    multi_tenant_users = users.sample(10)
+    multi_tenant_users.each do |user|
+      # 既に所属しているテナント以外から1〜2個選択
+      current_tenant_ids = user.tenant_memberships.pluck(:tenant_id)
+      available_tenants = tenants.reject { |t| current_tenant_ids.include?(t.id) }
+
+      next if available_tenants.empty?
+
+      additional_tenant_count = rand(1..2)
+      selected_tenants = available_tenants.sample(additional_tenant_count)
+
+      selected_tenants.each do |tenant|
+        # このテナント内で未使用の表示名を選択
+        used_names = TenantMembership.where(tenant: tenant).pluck(:display_name)
+        available_names = display_names - used_names
+        display_name = available_names.sample || "ユーザー#{rand(1000..9999)}"
+
+        TenantMembership.create!(
+          tenant: tenant,
+          user: user,
+          display_name: display_name
+        )
+      end
     end
   end
 
@@ -130,6 +193,7 @@ class SampleCreator
   def put_records
     puts_tenants
     puts_users
+    puts_tenant_memberships
     puts_moderators
     puts_topics
     puts_comments
@@ -148,6 +212,32 @@ class SampleCreator
   def puts_users
     User.find_each do |user|
       puts "User: #{user.nickname}, Suspended: #{user.suspended? ? 'Yes' : 'No'}"
+    end
+  end
+
+  def puts_tenant_memberships
+    puts 'Tenant Memberships'
+    Tenant.includes(tenant_memberships: :user).find_each do |tenant|
+      puts "テナント: #{tenant.name}"
+      tenant.tenant_memberships.each do |membership|
+        puts "  - #{membership.display_name} (#{membership.user.nickname})"
+      end
+      puts "  合計: #{tenant.tenant_memberships.count}人"
+      puts ''
+    end
+
+    # マルチテナント所属ユーザーの表示
+    puts 'マルチテナント所属ユーザー'
+    User.joins(:tenant_memberships)
+        .group('users.id')
+        .having('COUNT(tenant_memberships.id) > 1')
+        .includes(tenant_memberships: :tenant)
+        .find_each do |user|
+      puts "ユーザー: #{user.nickname}"
+      user.tenant_memberships.each do |membership|
+        puts "  - #{membership.tenant.name}: #{membership.display_name}"
+      end
+      puts ''
     end
   end
 

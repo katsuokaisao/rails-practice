3de960b01be3b0ee0233dc0a4f8db6e35dce3b2f
“katsuokaisao” <isaoda0@icloud.com>
2025-11-04 16:15:07 +0900

    topic, comment, comment historyのポリシーのマルチテナント対応

---
+++ app/policies/tenants/topics_policy.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Tenants
+  class TopicsPolicy < ApplicationPolicy
+    def index? = true
+    def show? = true
+    def new? = tenant_member? && unsuspended_user?
+    def create? = tenant_member? && unsuspended_user?
+    def edit? = tenant_member? && unsuspended_user? && owner?
+    def update? = tenant_member? && unsuspended_user? && owner?
+  end
+end

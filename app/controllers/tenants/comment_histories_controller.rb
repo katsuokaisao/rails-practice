c57a849428cff88bb2c93bdf611ca63c786e33bf
“katsuokaisao” <isaoda0@icloud.com>
2025-11-08 13:38:09 +0900

    コメント履歴一覧で範囲外のリクエストの際のリダイレクト先を直す

--- app/controllers/tenants/comment_histories_controller.rb
+++ app/controllers/tenants/comment_histories_controller.rb
@@ -14,7 +14,7 @@ module Tenants
       return unless @pagination.out_of_bounds
 
       flash[:alert] = t('flash.actions.out_of_bounds')
-      redirect_to tenant_path(tenant_slug: current_tenant.identifier)
+      redirect_to tenant_comment_histories_path(tenant_slug: current_tenant.identifier, comment_id: @comment.id)
     end
 
     def compare

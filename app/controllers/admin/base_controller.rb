87c1e60ba9f390419656dfde70129682e9a7cb06
“katsuokaisao” <isaoda0@icloud.com>
2025-11-25 00:39:47 +0900

    テナント識別子の検証を更新し、存在しない場合の挙動を404エラーに変更

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,6 +2,8 @@
 
 module Admin
   class BaseController < ApplicationController
+    before_action :set_current_tenant
+
     def set_current_tenant
       @current_tenant = Tenant.find(params[:id])
     end

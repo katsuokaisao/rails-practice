60ab35340f8921e127b44797f1e6a8c15c91ed80
“katsuokaisao” <isaoda0@icloud.com>
2025-11-29 11:06:10 +0900

    リファクタリング: チェックボックスの選択/非選択処理をtoggleAllCheckboxesメソッドに統合

--- app/javascript/controllers/unsubscription_controller.js
+++ app/javascript/controllers/unsubscription_controller.js
@@ -4,39 +4,32 @@ export default class extends Controller {
   static targets = ["checkbox", "submitButton"]
 
   connect() {
-    console.debug("Unsubscription controller connected")
     this.updateSubmitButton()
   }
 
   selectAll(event) {
     event.preventDefault()
-    this.checkboxTargets.forEach(checkbox => {
-      checkbox.checked = true
-    })
-    this.updateSubmitButton()
+    this.toggleAllCheckboxes(true)
   }
 
   deselectAll(event) {
     event.preventDefault()
-    this.checkboxTargets.forEach(checkbox => {
-      checkbox.checked = false
-    })
-    this.updateSubmitButton()
+    this.toggleAllCheckboxes(false)
   }
 
   updateSubmitButton() {
-    if (this.hasSubmitButtonTarget) {
-      const hasChecked = this.checkboxTargets.some(checkbox => checkbox.checked)
-      this.submitButtonTarget.disabled = !hasChecked
-    }
+    if (!this.hasSubmitButtonTarget) return
+
+    const hasChecked = this.checkboxTargets.some(checkbox => checkbox.checked)
+    this.submitButtonTarget.disabled = !hasChecked
   }
 
   confirmSubmit(event) {
     const checkedCount = this.checkboxTargets.filter(cb => cb.checked).length
 
     if (checkedCount === 0) {
-      alert("一つ以上のテナントを選択してください。")
       event.preventDefault()
+      alert("一つ以上のテナントを選択してください。")
       return
     }
 
@@ -46,5 +39,12 @@ export default class extends Controller {
     if (!confirm(message)) {
       event.preventDefault()
     }
+  }
+
+  toggleAllCheckboxes(checked) {
+    this.checkboxTargets.forEach(checkbox => {
+      checkbox.checked = checked
+    })
+    this.updateSubmitButton()
   }
 }

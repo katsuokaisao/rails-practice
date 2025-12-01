bf5b53b2fc989096f4138d081e7a356a3b692e8a
“katsuokaisao” <isaoda0@icloud.com>
2025-12-02 00:25:00 +0900

    topic_locked?メソッドをApplicationPolicyではなく、各ポリシーで定義する

--- app/policies/tenants/comments_policy.rb
+++ app/policies/tenants/comments_policy.rb
@@ -5,5 +5,12 @@ module Tenants
     def create? = active_tenant_member? && unsuspended_user? && !topic_locked?
     def edit? = active_tenant_member? && unsuspended_user? && owner? && !topic_locked?
     def update? = active_tenant_member? && unsuspended_user? && owner? && !topic_locked?
+
+    private
+
+    def topic_locked?
+      topic = record.is_a?(Topic) ? record : record.topic
+      topic.locked?
+    end
   end
 end
--- app/policies/tenants/topics_policy.rb
+++ app/policies/tenants/topics_policy.rb
@@ -7,5 +7,9 @@ module Tenants
     def create? = active_tenant_member? && unsuspended_user?
     def edit? = active_tenant_member? && unsuspended_user? && owner? && !topic_locked?
     def update? = active_tenant_member? && unsuspended_user? && owner? && !topic_locked?
+
+    private
+
+    def topic_locked? = record.locked?
   end
 end

ef203dfcc783b6227e7e14ed573c306aa0e48485
“katsuokaisao” <isaoda0@icloud.com>
2025-12-01 09:49:42 +0900

    strong parameterを使うように修正

--- app/controllers/unsubscriptions_controller.rb
+++ app/controllers/unsubscriptions_controller.rb
@@ -10,7 +10,7 @@ class UnsubscriptionsController < ApplicationController
   end
 
   def create
-    tenant_ids = params[:tenant_ids] || []
+    tenant_ids = unsubscription_params[:tenant_ids] || []
 
     if tenant_ids.empty?
       redirect_to new_unsubscription_path, alert: t('.no_tenants_selected')
@@ -30,5 +30,11 @@ class UnsubscriptionsController < ApplicationController
     UnsubscriptionJob.perform_later(current_user.id, valid_tenants.pluck(:tenant_id))
 
     redirect_to new_unsubscription_path, notice: t('.success')
+  end
+
+  private
+
+  def unsubscription_params
+    params.permit(tenant_ids: [])
   end
 end
